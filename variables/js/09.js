2ac70cb0269b0864cbe444c23592b778cfa9989b
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-06 20:56:56 -0500

    arrays review

---
+++ variables/js/09.js
@@ -0,0 +1,14 @@
+// Arrays- Destructuring 
+const tecnologias = ["CSS", "HTML", "JAVASCRIPT", "REACT", "NODE"]
+
+// const [html, react, node] = tecnologias
+// console.log(html, react, node)
+
+const [ , , , , var5] = tecnologias //Para extraer solo una posicion y no nombrarlas todas 
+console.log(var5)
+
+
+
+
+
+
