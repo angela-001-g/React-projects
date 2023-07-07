ff8be334a0f4cae44a9b9b222153ab4ff4d57389
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-06 22:48:23 -0500

    functions

---
+++ variables/js/11.js
@@ -0,0 +1,13 @@
+// Function expression 
+
+// Function ddeclaration: Puedo llamarla antes de declararla 
+function suma(num1, num2){
+    return num1 + num2
+}
+
+// Function expression: No puedo llamarla antes de declararla
+const resta = function(num3, num4){
+    return num3 - num4
+}
+
+
