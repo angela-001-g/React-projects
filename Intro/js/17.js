868abdda4229fa488a7c0baffbb607b96d7eb5c6
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-11 19:43:00 -0500

    fix mistake

--- Intro/js/17.js
+++ Intro/js/17.js
@@ -1,5 +1,5 @@
 // Imports y exports
-import cualquierNombre from "./functions"
+import cualquierNombre from "./functions.js" //Nativamente, sí pide el .js, en React no 
 
  const resultado = cualquierNombre(20, 20)
  console.log(resultado)
